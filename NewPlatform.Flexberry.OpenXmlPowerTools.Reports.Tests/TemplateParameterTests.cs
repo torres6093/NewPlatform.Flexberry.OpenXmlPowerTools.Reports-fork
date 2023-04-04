e582b9c84174f94856158db1a7252ab9237d6d1c
Andrey Chibisov <achibisov@skyori.ru>
2023-04-04 16:08:32 +0500

    Test

--- NewPlatform.Flexberry.OpenXmlPowerTools.Reports.Tests/TemplateParameterTests.cs
+++ NewPlatform.Flexberry.OpenXmlPowerTools.Reports.Tests/TemplateParameterTests.cs
@@ -9,6 +9,7 @@ namespace NewPlatform.Flexberry.OpenXmlPowerTools.Reports.Tests
         [Fact]
         public void TemplateParameter_ParseNameAndFormat_FormatDateTime()
         {
+            // Test
             var subject = new TemplateParameter("ConstructorParameterName:dd-MM-yyyy");
             var dateTime = new DateTime(2021, 1, 1);
             string result = subject.FormatObject(dateTime);
